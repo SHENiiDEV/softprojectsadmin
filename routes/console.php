177c4dac6b0e4f32f096e1afee9190400afae446
Ambex Developer <developer.7n5x@ambex.lv>
2026-08-27 12:07:31 +0300

    Set application timezone to Europe/Riga in config and scheduled commands so daily digest arrives at 9:00 AM Riga time

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reports:check-deadlines')->daily();
-Schedule::command('tasks:check-deadlines')->dailyAt('09:00');
-Schedule::command('digest:daily')->dailyAt('09:00');
-Schedule::command('gmail:watch')->dailyAt('04:00');
+Schedule::command('reports:check-deadlines')->daily()->timezone('Europe/Riga');
+Schedule::command('tasks:check-deadlines')->dailyAt('09:00')->timezone('Europe/Riga');
+Schedule::command('digest:daily')->dailyAt('09:00')->timezone('Europe/Riga');
+Schedule::command('gmail:watch')->dailyAt('04:00')->timezone('Europe/Riga');
 Schedule::command('gmail:sync-recent')->everyTwoMinutes();
